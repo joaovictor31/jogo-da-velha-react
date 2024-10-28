1b8fcd57705ea87819383d728eba3163527f3c2a
João <ut7vitor60@gmail.com>
2024-10-28 17:03:52 -0300

    Adicionando campo de formulário ao clicar

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react"
+
 export default function Player({name, symbol}){
+      // isEditing inicia como false (não estamos no modo de edição)
+    const [isEditing, setIsEditing] = useState(false);
+    
+    function active(){
+        setIsEditing(true) // muda isEditing para true ao clicar em Edit
+    }
+
+    console.log(isEditing); // mostra o valor atual de isEditing no console
+
+
     return(
         <li>
         <span className="player">
-            <span className="player-name">{name}</span>
+            {/* Só mostra o name quando isEditing é falso */}
+         {!isEditing && <span className="player-name">{name}</span>}
+         {isEditing && <span><input type="text" required/></span>}
             <span className="player-symbol">{symbol}</span>
         </span>
-        <button>Edit</button>
+        <button onClick={active}>Edit</button>
+         {/* Não preciso executar a função, apenas colocar o valor dela*/}
       
       </li>
     )
